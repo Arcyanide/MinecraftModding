f4a76c2c35d7011d7e244f3c0102275406380b61
Arcyanide <gamerlifeforever456@gmail.com>
2025-04-02 01:21:09 -0600

    new stuff

--- src/main/java/net/damian/tutorialmod/TutorialMod.java
+++ src/main/java/net/damian/tutorialmod/TutorialMod.java
@@ -20,7 +20,6 @@ public class TutorialMod {
     public static final String MOD_ID = "tutorialmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
